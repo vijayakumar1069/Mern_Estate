e88bf59103de07207307dd5c9775860bbfde0576
vijayakumar1069 <vijay.r20799@gmail.com>
2023-11-21 15:15:13 +0530

    mern estate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./Componenet/Header";
 
 export default function App() {
   return (
-    <div className=''>App with co</div>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signout" element={<SignOut />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
